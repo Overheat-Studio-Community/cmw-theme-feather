cadf5403f83c737acb69980baa2b7c10fe617d2d
SuiramX <marius.bernard1@ecoles-epsi.net>
2025-01-07 17:53:23 +0100

    [IMPROVES] Cards mis à jour [FIX] Bug sur navbar

--- Views/Core/home.view.php
+++ Views/Core/home.view.php
@@ -7,29 +7,50 @@ use CMW\Utils\Website;
 Website::setTitle('Accueil');
 Website::setDescription("page d'accueil de CraftMyWebsite");
 ?>
-<style>
-    body {
-        margin: 10px;
-        padding: 0;
-        background-color: white;
-        background-size: cover;
-    }
-    img {
-        margin-top: -65px;
-        padding: 0;
-        max-height: 768px;
+    <style>
+        body {
+            margin: 10px;
+            padding: 0;
+            background-color: white;
+            background-size: cover;
+        }
+        img {
+            max-height: 768px;
+        }
+        .bubble {
+            position: absolute;
+            top: 10px;
+            left: 10px;
+            background-color: gray;
+            color: white;
+            padding: 5px 10px;
+            border-radius: 20px;
+            font-size: 12px;
+        }
+        .container {
+            position: relative;
+        }
+    </style>
 
-    }
-</style>
-
-<img alt="Background" src="<?= EnvManager::getInstance()->getValue("PATH_SUBFOLDER") . 'Public/Themes/Feather/Assets/Img/Background.jpg' ?>">
+    <img alt="Background" src="<?= EnvManager::getInstance()->getValue("PATH_SUBFOLDER") . 'Public/Themes/Feather/Assets/Img/vetements.png' ?>"
+<h2 class="text-3xl mt-6">
+    Blog
+</h2>
+<h3 class="text-xl mt-3 text-gray-600">
+    Vous trouverez ici les 9 derniers blogs disponibles et mis en ligne.
+</h3>
+        <nav class="flex justify-between p-4 rounded-lg shadow-md">
+            <a href="#" class="rounded-lg bg-gray-300 px-4 py-2 text-black">All</a>
+        </nav>
+    </div>
 <?php for ($j = 1; $j <= 3; $j++): ?>
     <div class="flex justify-between gap-4">
         <?php for ($i = 1; $i <= 3; $i++): ?>
-            <div class="max-w-sm bg-white shadow-md rounded-lg overflow-hidden mx-auto">
+            <div class="max-w-sm bg-white shadow-md rounded-lg overflow-hidden mx-auto container">
+                <div class="bubble">Mode</div>
                 <img class="w-full h-48 object-cover" src="<?= EnvManager::getInstance()->getValue("PATH_SUBFOLDER") . 'Public/Themes/Feather/Assets/Img/vetements.png'?>" alt="Packing Tips">
                 <div class="p-4">
-                    <span class="text-sm text-gray-500 bg-gray-100 px-2 py-1 rounded">Showroom</span>
+                    <span class="text-sm text-gray-500 bg-gray-100 px-2 py-1 rounded"><?= date('z-M-Y') ?> . 6 minutes read </span>
                     <h2 class="text-lg font-semibold text-gray-800 mt-2">
                         Mon dressing
                     </h2>
@@ -37,12 +58,12 @@ Website::setDescription("page d'accueil de CraftMyWebsite");
                         Découvrez mon dressing sous tous ses ensembles
                     </p>
                     <div class="flex items-center mt-4">
-                        <div class="flex-shrink-0">
-                            <img class="w-10 h-10 rounded-full" src="<?= EnvManager::getInstance()->getValue("PATH_SUBFOLDER") . 'Public/Themes/Feather/Assets/Img/pp-texier.png'?>" alt="Author">
+                        <div class="flex-shrink-0 w-10 h-10">
+                            <img class="w-10 h-10 rounded-full" src="<?= EnvManager::getInstance()->getValue("PATH_SUBFOLDER") . 'Public/Themes/Feather/Assets/Img/pp-basic.jpg'?>" alt="Author">
                         </div>
                         <div class="ml-3">
-                            <p class="text-sm font-medium text-gray-800">Thomas Texier</p>
-                            <p class="text-sm text-gray-500">7 Janvier 2025</p>
+                            <p class="text-sm font-medium text-gray-800 ">Some1</p>
+                            <p class="text-sm text-gray-500"></p>
                         </div>
                     </div>
                 </div>
@@ -50,11 +71,3 @@ Website::setDescription("page d'accueil de CraftMyWebsite");
         <?php endfor; ?>
     </div>
 <?php endfor; ?>
-
-
-
-
-
-
-
-
--- Views/Includes/header.inc.php
+++ Views/Includes/header.inc.php
@@ -13,7 +13,7 @@ $menus = MenusModel::getInstance();
         <?= Website::getWebsiteName() ?>
     </div>
 
-    <ul class="flex space-x-4">
+    <ul class="hidden lg:flex space-x-4">
         <?php foreach ($menus->getMenus() as $menus): ?>
             <?php if ($menus->isUserAllowed()): ?>
                 <li <?php if ($menus->urlIsActive()) {
@@ -25,22 +25,75 @@ $menus = MenusModel::getInstance();
             <?php endif; ?>
         <?php endforeach; ?>
     </ul>
+    <div class="flex lg:hidden flex flex-col justify-center bg-white">
+        <div class="relative">
+            <nav x-data="{ open: false }">
+                <!-- Bouton burger -->
+                <button class="text-gray-500 w-10 h-10 relative focus:outline-none" @click="open = !open">
+                    <span class="sr-only">Open main menu</span>
+                    <div class="block w-5 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                        <span aria-hidden="true"
+                              class="block absolute h-0.5 w-5 bg-current transform transition duration-500 ease-in-out"
+                              :class="{'rotate-45': open,' -translate-y-1.5': !open }"></span>
+                        <span aria-hidden="true"
+                              class="block absolute h-0.5 w-5 bg-current transform transition duration-500 ease-in-out"
+                              :class="{'opacity-0': open }"></span>
+                        <span aria-hidden="true"
+                              class="block absolute h-0.5 w-5 bg-current transform transition duration-500 ease-in-out"
+                              :class="{'-rotate-45': open, ' translate-y-1.5': !open}"></span>
+                    </div>
+                </button>
+                <!-- Menu -->
+                <ul x-show="open" x-transition class="flex flex-col space-y-4 mt-4">
 
-    <div class="flex items-center gap-4">
-        <?php if (UsersController::isUserLogged() && UsersController::isAdminLogged()): ?>
-            <div>
-                <a href="<?= EnvManager::getInstance()->getValue('PATH_SUBFOLDER') ?>cmw-admin" target="_blank"
-                   class="bg-blue-500 hover:bg-blue-600 py-2 px-4 rounded">
-                    Panel Admin
-                </a>
-            </div>
-        <?php else: ?>
-            <div>
-                <a href="<?= EnvManager::getInstance()->getValue('PATH_SUBFOLDER') ?>login"
-                   class="bg-blue-500 hover:bg-blue-600 py-2 px-4 rounded">
-                    Connexion
-                </a>
-            </div>
-        <?php endif; ?>
+                    <li><a href="#" class="text-gray-500">Home</a></li>
+                    <li><a href="#" class="text-gray-500">About</a></li>
+                    <li><a href="#" class="text-gray-500">Services</a></li>
+                    <li><div>
+                            <a href="<?= EnvManager::getInstance()->getValue('PATH_SUBFOLDER') ?>cmw-admin"
+                               class="bg-white hover:bg-white text-blue-500 py-2 px-4 rounded">
+                                Sign Up
+                            </a>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <a href="<?= EnvManager::getInstance()->getValue('PATH_SUBFOLDER') ?>login"
+                               class="bg-blue-500 hover:bg-blue-600 py-2 px-4 rounded">
+                                Connexion
+                            </a>
+                        </div></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+    <div class="hidden lg:flex lg:gap-x-12">
+        <input
+            type="search"
+            class="relative m-0 block flex-auto rounded border border-solid border-neutral-200 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-surface outline-none transition duration-200 ease-in-out placeholder:text-neutral-500 focus:z-[3] focus:border-primary focus:shadow-inset focus:outline-none motion-reduce:transition-none dark:border-white/10 dark:text-white dark:placeholder:text-neutral-200 dark:autofill:shadow-autofill dark:focus:border-primary"
+            placeholder="Search"
+            aria-label="Search"
+            id="exampleFormControlInput2"
+            aria-describedby="button-addon2"/>
+        <span
+            class="flex items-center whitespace-nowrap px-3 py-[0.25rem] text-surface dark:border-neutral-400 dark:text-white [&>svg]:h-5 [&>svg]:w-5"
+            id="button-addon2">
+   <i class="fa-solid fa-magnifying-glass"></i>
+  </span>
+    </div>
+    <div class="hidden lg:flex flex items-center gap-4">
+        <div>
+            <a href="<?= EnvManager::getInstance()->getValue('PATH_SUBFOLDER') ?>cmw-admin"
+               class="bg-white hover:bg-white text-blue-500 py-2 px-4 rounded">
+                Sign Up
+            </a>
+        </div>
+        <div>
+            <a href="<?= EnvManager::getInstance()->getValue('PATH_SUBFOLDER') ?>login"
+               class="bg-blue-500 hover:bg-blue-600 py-2 px-4 rounded">
+                Connexion
+            </a>
+        </div>
     </div>
 </nav>
