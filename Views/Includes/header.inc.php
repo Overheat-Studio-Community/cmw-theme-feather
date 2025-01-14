837f4a007aedb2415f9b6221010a877169b61f69
SuiramX <marius.bernard1@ecoles-epsi.net>
2025-01-14 17:02:45 +0100

    [CLEAN] Clean Code Navbar v3 ( Bug connexion donc repush )

--- Views/Includes/header.inc.php
+++ Views/Includes/header.inc.php
@@ -6,6 +6,7 @@ use CMW\Manager\Env\EnvManager;
 use CMW\Model\Core\MenusModel;
 use CMW\Utils\Website;
 
+
 $subfolder = EnvManager::getInstance()->getValue('PATH_SUBFOLDER');
 $usersession =UsersSessionsController::getInstance()->getCurrentUser();
 $userlog = UsersController::isUserLogged();
