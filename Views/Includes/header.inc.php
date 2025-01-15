3709cb719fbc592440c377c0bf3677a94ca30380
SuiramX <marius.bernard1@ecoles-epsi.net>
2025-01-15 10:52:10 +0100

    [FIX] Déroulage burger sous mobile.

--- Views/Includes/header.inc.php
+++ Views/Includes/header.inc.php
@@ -8,7 +8,7 @@ use CMW\Utils\Website;
 
 
 $subfolder = EnvManager::getInstance()->getValue('PATH_SUBFOLDER');
-$usersession =UsersSessionsController::getInstance()->getCurrentUser();
+$usersession = UsersSessionsController::getInstance()->getCurrentUser();
 $userlog = UsersController::isUserLogged();
 $menus = MenusModel::getInstance();
 ?>
@@ -36,13 +36,14 @@ $menus = MenusModel::getInstance();
              id="search-container">
 
             <input type="search"
-                class="hidden sm:flex sm:flex-grow px-4 py-2 text-black focus:outline-none transition-all duration-300 max-w-[200px] w-full"
-                placeholder="Search" aria-label="Search" id="search-input" aria-describedby="button-addon2"/>
+                   class="hidden sm:flex sm:flex-grow px-4 py-2 text-black focus:outline-none transition-all duration-300 max-w-[200px] w-full"
+                   placeholder="Search" aria-label="Search" id="search-input" aria-describedby="button-addon2"/>
             <span class="hidden sm:flex items-center justify-center px-4 cursor-pointer">
                 <i class="fa-solid fa-magnifying-glass text-black"></i>
             </span>
 
-            <span class="sm:hidden flex items-center justify-center px-4 cursor-pointer" id="button-addon2" onclick="toggleSearchBar()">
+            <span class="sm:hidden flex items-center justify-center px-4 cursor-pointer" id="button-addon2"
+                  onclick="toggleSearchBar()">
                 <i class="fa-solid fa-magnifying-glass text-black"></i>
             </span>
             <!-- Croix de fermeture -->
@@ -110,22 +111,29 @@ $menus = MenusModel::getInstance();
                     <!-- Menu -->
                     <div id="burger-menu" class="hidden w-full sm:transform absolute top-15 right-0">
                         <ul class="w-full sm:flex-row sm:max-w-[16rem] sm:ml-auto flex-col space-y-4 bg-white p-4 shadow-lg rounded right-0">
-                            <div class="justify-center justify-content-center gap-2 text-black">
-                                <a href="<?= $menu->getUrl() ?>" <?= !$menu->isTargetBlank() ?: "target='_blank'" ?>
-                                   class="hover:text-gray-300"><?= $menu->getName() ?></a>
-                                <?php if (!$userlog): ?>
-                                    <div class="flex mt-4 gap-2 md:hidden">
-                                        <a href="<?= $subfolder ?>register"
-                                           class="bg-white/50 text-blue-500 py-2 px-4 rounded">
-                                            Sign Up
-                                        </a>
-                                        <a href="<?= $subfolder ?>login"
-                                           class="bg-blue-500 hover:bg-blue-600 py-2 px-4 rounded">
-                                            Connexion
-                                        </a>
-                                    </div>
+
+                            <?php foreach ($menus->getMenus() as $menu): ?>
+                                <?php if ($menu->isUserAllowed()): ?>
+                                    <li>
+                                        <a href="<?= $menu->getUrl() ?>" <?= !$menu->isTargetBlank() ?: "target='_blank'" ?>
+                                           class="text-black"><?= $menu->getName() ?></a>
+                                    </li>
                                 <?php endif; ?>
-                            </div>
+                            <?php endforeach; ?>
+                            <?php if (!$userlog): ?>
+                                <li>
+                                    <a href="<?= $subfolder ?>register"
+                                       class="flex md:hidden bg-white/50 text-blue-500 py-2 px-4 rounded">
+                                        Sign Up
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="<?= $subfolder ?>login"
+                                       class="flex md:hidden bg-blue-500 hover:bg-blue-600 py-2 px-4 rounded">
+                                        Connexion
+                                    </a>
+                                </li>
+                            <?php endif; ?>
                         </ul>
                     </div>
                 </nav>
@@ -135,66 +143,64 @@ $menus = MenusModel::getInstance();
 </nav>
 <script>
     document.addEventListener('DOMContentLoaded', () => {
-        // Gestion de l'affichage du menu avec animation
         const toggle = document.getElementById('profile-toggle');
         const menu = document.getElementById('profile-menu');
+        if (toggle && menu) {
+            toggle.addEventListener('click', () => {
+                menu.classList.toggle('hidden');
+                menu.classList.toggle('scale-95');
+                menu.classList.toggle('opacity-0');
+            });
 
-        toggle.addEventListener('click', () => {
-            menu.classList.toggle('hidden');
-            menu.classList.toggle('scale-95');
-            menu.classList.toggle('opacity-0');
-        });
+            document.addEventListener('click', (event) => {
+                if (!toggle.contains(event.target) && !menu.contains(event.target)) {
+                    menu.classList.add('hidden', 'scale-95', 'opacity-0');
+                }
+            });
+        }
 
-        // Cacher le menu lorsque l'utilisateur clique ailleurs
-        document.addEventListener('click', (event) => {
-            if (!toggle.contains(event.target) && !menu.contains(event.target)) {
-                menu.classList.add('hidden', 'scale-95', 'opacity-0');
-            }
-        });
-
-        // Afficher/masquer le menu burger
         const burgerToggle = document.getElementById('burger-toggle');
         const burgerMenu = document.getElementById('burger-menu');
+        if (burgerToggle && burgerMenu) {
+            burgerToggle.addEventListener('click', () => {
+                burgerMenu.classList.toggle('hidden');
+            });
 
-        burgerToggle.addEventListener('click', () => {
-            burgerMenu.classList.toggle('hidden');
-        });
+            document.addEventListener('click', (event) => {
+                if (!burgerToggle.contains(event.target) && !burgerMenu.contains(event.target)) {
+                    burgerMenu.classList.add('hidden');
+                }
+            });
+        }
 
-        // Cacher le menu burger si l'utilisateur clique ailleurs
-        document.addEventListener('click', (event) => {
-            if (!burgerToggle.contains(event.target) && !burgerMenu.contains(event.target)) {
-                burgerMenu.classList.add('hidden');
-            }
-        });
-
-        // Fonction pour afficher la barre de recherche
         const searchToggle = document.getElementById('button-addon2');
         const searchInput = document.getElementById('search-input');
         const closeBtn = document.getElementById('close-search-btn');
         const navbarElements = document.getElementById('navbar-elements');
         const searchContainer = document.getElementById('search-container');
 
-        // Afficher la barre de recherche et cacher les boutons profile-toggle et burger-toggle
-        searchToggle.addEventListener('click', () => {
-            searchInput.classList.remove('hidden');
-            searchInput.focus();
-            closeBtn.classList.remove('hidden');
-            searchContainer.classList.add('absolute', 'top-0', 'left-0', 'w-full', 'bg-white', 'opacity-100');
-            navbarElements.classList.add('hidden');
-            burgerMenu.classList.add('hidden');
-            toggle.classList.add('hidden');
-            burgerToggle.classList.add('hidden');
-        });
+        if (searchToggle && searchInput && closeBtn && navbarElements && searchContainer) {
+            searchToggle.addEventListener('click', () => {
+                searchInput.classList.remove('hidden');
+                searchInput.focus();
+                closeBtn.classList.remove('hidden');
+                searchContainer.classList.add('absolute', 'top-0', 'left-0', 'w-full', 'bg-white', 'opacity-100');
+                navbarElements.classList.add('hidden');
+                burgerMenu?.classList.add('hidden');
+                toggle?.classList.add('hidden');
+                burgerToggle?.classList.add('hidden');
+            });
 
-        // Fermer la barre de recherche et réafficher les boutons profile-toggle et burger-toggle
-        closeBtn.addEventListener('click', () => {
-            searchInput.classList.add('hidden');
-            closeBtn.classList.add('hidden');
-            searchContainer.classList.remove('absolute', 'top-0', 'left-0', 'w-full', 'bg-white', 'opacity-100');
-            navbarElements.classList.remove('hidden');
-            burgerMenu.classList.remove('hidden');
-            toggle.classList.remove('hidden');
-            burgerToggle.classList.remove('hidden');
-        });
+            closeBtn.addEventListener('click', () => {
+                searchInput.classList.add('hidden');
+                closeBtn.classList.add('hidden');
+                searchContainer.classList.remove('absolute', 'top-0', 'left-0', 'w-full', 'bg-white', 'opacity-100');
+                navbarElements.classList.remove('hidden');
+                burgerMenu?.classList.remove('hidden');
+                toggle?.classList.remove('hidden');
+                burgerToggle?.classList.remove('hidden');
+            });
+        }
     });
+
 </script>
