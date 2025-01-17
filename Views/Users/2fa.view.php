9a6fef53ee22dec883fd18475b108afe6ed9a8d1
SuiramX <marius.bernard1@ecoles-epsi.net>
2025-01-17 17:58:40 +0100

    [UPLOADS] 2FA / Forgot password / Profile [FIX] header/head/footer full width Commit de fin de journée !

--- Views/Users/2fa.view.php
+++ Views/Users/2fa.view.php
@@ -8,10 +8,27 @@ Website::setTitle('Connexion - 2FA');
 Website::setDescription('Double authentification');
 ?>
 
+<div class="flex-grow flex items-center justify-center">
+    <div class="my-2 flex flex-col border border-gray-100 rounded w-full max-w-md">
+        <h1 class="mt-2 flex justify-center">Connexion en double authentification</h1>
+        <form class="flex flex-col" action=""
+              action="<?= EnvManager::getInstance()->getValue('PATH_SUBFOLDER') . 'login/validate/tfa' ?>"
+              method="post">
+            <?php SecurityManager::getInstance()->insertHiddenToken() ?>
+            <div class="relative w-full max-w-sm w-full mx-auto mt-4 my-2">
+                <label for="mt-4 my-2">Code d'authentification</label>
+                <input class="relative w-full bg-gray-100 rounded-lg pl-2 py-2 my-2"
+                       id="code"
+                       name="code"
+                       type="text"
+                       placeholder="123456"
+                       maxlength="7"
+                       required>
 
-<form action="<?= EnvManager::getInstance()->getValue('PATH_SUBFOLDER') . 'login/validate/tfa' ?>" method="post">
-    <?php SecurityManager::getInstance()->insertHiddenToken() ?>
-    <input id="code" class="form-control" name="code" type="text" placeholder="123456" maxlength="7" required>
-    <label for="code">Code d'authentification</label>
-    <button type="submit">Connexion</button>
-</form>
+                <button class="mb-2 flex items-center justify-center bg-black text-white rounded-lg pl-2 py-2 w-full"
+                        type="submit">Connexion
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
--- Views/Users/forgot_password.view.php
+++ Views/Users/forgot_password.view.php
@@ -7,14 +7,26 @@ use CMW\Utils\Website;
 Website::setTitle('Mot de passe oublié');
 Website::setDescription('Retrouvez votre mot de passe');
 ?>
+<div class="flex-grow flex items-center justify-center">
+    <div class="my-2 flex flex-col border border-gray-100 rounded w-full max-w-md">
+        <h1 class="mt-2 flex justify-center">Mot de passe oublié</h1>
+        <form class="flex flex-col" action="" method="post">
+            <?php SecurityManager::getInstance()->insertHiddenToken() ?>
+            <div class="relative w-full max-w-sm w-full mx-auto mt-4 my-2">
+                <label for="email">Email</label>
+                <input
+                    class="relative w-full bg-gray-100 rounded-lg pl-2 py-2 my-2"
+                    type="email"
+                    name="mail"
+                    id="mail"
+                    placeholder="<?= LangManager::translate('users.users.mail') ?>"
+                />
 
-<h1>Mot de passe oublié</h1>
-<form action="" method="post">
-    <?php SecurityManager::getInstance()->insertHiddenToken() ?>
-    <div>
-        <label for="email">Email : </label>
-        <input type="email" name="mail" id="mail" placeholder="<?= LangManager::translate('users.users.mail') ?>">
+                <button
+                    class="mb-2 flex items-center justify-center bg-black text-white rounded-lg pl-2 py-2 w-full"
+                    type="submit">Envoyer
+                </button>
+            </div>
+        </form>
     </div>
-    <button type="submit"><?= LangManager::translate('users.login.forgot_password.btn') ?></button>
-</form>
-
+</div>
